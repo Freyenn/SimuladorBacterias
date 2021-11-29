92fc4dfbd3a13347cd8ed79089ff138dd8fc2bd9
Freyen <luis-efrain@hotmail.com>
2021-11-29 16:28:22 -0600

    actualizacion

--- static/python/rbf.py
+++ static/python/rbf.py
@@ -4,7 +4,7 @@ Created on Fri Oct 22 15:35:40 2021
 
 @author: luis-
 """
-from kmeans import KMEANS
+
 import numpy as np
 
 class rbf():
@@ -18,52 +18,7 @@ class rbf():
         self.W = Wnew
         self.k=k
         
-    def entrenamiento(self,x,y):
-        #Etapa no supervisada
-        self.x=x
-        #inicializar el clasificador
-        clasificador = KMEANS(self.k)
-        #agrupar datos
-        c,grupos = clasificador.AGRUPAR(x)
-        self.c=c.T
-        #Calcular sigma
-        distancias_c = np.zeros((self.c.shape[0],self.x.shape[1]))
-        for i in range(self.c.shape[0]):
-            for j in range(self.x.shape[1]):
-                distancias_c[i,j]=EUCLIDIANA(self.c[i,:],self.x[:,j])
-                
-        sigma = np.zeros((distancias_c.shape[0]))
-        for i in range(distancias_c.shape[0]):        
-            sigma[i]=(np.amax(distancias_c[i,:])-np.amin(distancias_c[i,:]))/np.sqrt(2*self.k)
-                
-        #sigma = (max(distancias_c)-min(distancias_c))/np.sqrt(2*self.k)
-        print("Sigma",sigma)
-        #self.sigma = sigma[0]
-        self.sigma = sigma
-# =============================================================================
-#         #matriz G
-#         G=np.zeros((x.shape[1],self.k))
-#         for i in range(x.shape[1]):
-#             for j in range(self.k):
-#                 dist = np.linalg.norm(x[:,i]-self.c[j],2)
-#                 G[i,j] = np.exp((-1/self.sigma**2) * dist**2)
-#                 
-#         self.W = np.dot(np.linalg.pinv(G),y.T)
-#         print("Tamaño W",self.W.shape)
-# =============================================================================
-# =============================================================================
-# Matriz G Multiple
-        G=np.zeros((x.shape[1],self.k))
-        for i in range(x.shape[1]):
-            for j in range(self.k):
-                dist = np.linalg.norm(x[:,i]-self.c[j,:],2)
-                G[i,j] = np.exp((-1/(self.sigma[j]**2)) * dist**2)
-                
-        print("Matriz G:",G)        
-        self.W = np.dot(np.linalg.pinv(G),y.T)
-        print("Tamaño W",self.W.shape)
-
-
+ 
 # =============================================================================
     def evaluar(self,entrada):
             G=np.zeros((entrada.shape[1],self.k))
